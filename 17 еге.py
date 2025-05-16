328c6fabb20aee43cead948cd713dcd7f103cabd
Redvarik <104436331+Redvarik@users.noreply.github.com>
2025-05-16 16:26:50 +0300

    Update 17 еге.py

--- 17 еге.py
+++ 17 еге.py
@@ -1,8 +1,10 @@
-a = 2476
-r = []
-for i in range(2476, 7857 + 1):
+a = 2461
+r = set()
+
+for i in range(2461, 9719 + 1):
     a += 1
-    if a % 2 == 0 and a % 8 != 0 and (a // 100) % 10 >= 7:
-        r.append(a)
+    a0 = oct(a)
+    if ((a//10) % 10) >= 3 and ((a//10) % 10) <= 7 and ((a // 100) % 10) != 1 and ((a // 100) % 10) != 9:
+        r.add(a)
 print(len(r))
-print((min(r)+max(r))/2)
+print(max(r))
